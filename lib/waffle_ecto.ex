809413b88105a92272c4acdf2fe84cf933e4d571
Boris Kuznetsov <achempion@gmail.com>
2019-10-03 02:41:08 +0300

    add documentation for URL generation

--- lib/waffle_ecto.ex
+++ lib/waffle_ecto.ex
@@ -1,3 +1,13 @@
 defmodule Waffle.Ecto do
+  @moduledoc """
+  Waffle.Ecto provides an integration with Waffle and Ecto.
+
+  Waffle attachments should be stored in a string column, with a name indicative of the attachment.
+
+    create table :users do
+      add :avatar, :string
+    end
+  """
+
   @type t :: %{file_name: String.t, updated_at: DateTime.t}
 end
